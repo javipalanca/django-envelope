7bd673783649b6c2efc7470cdf32a9e9504d06ab
Javi Palanca <jpalanca@gmail.com>
2024-08-08 21:48:32 +0200

    Update url to path

--- envelope/urls.py
+++ envelope/urls.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import url
+from django.urls import path
 
 from envelope.views import ContactView
 
 
 urlpatterns = [
-    url(r'^$', ContactView.as_view(), name='envelope-contact'),
+    path(r'^$', ContactView.as_view(), name='envelope-contact'),
 ]
